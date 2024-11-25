5f5ef58fb17b077a5e290f01ca5f9d77844f4bbf
jencba <jdamian@elam.co.uk>
2024-11-25 11:23:42 +0000

    first challenge complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,20 @@
 const endpointsJson = require("../endpoints.json");
+const app= require('../app')
+const request = require('supertest');
+const seed = require('../db/seeds/seed');
+const data = require('../db/data/test-data');
+const db = require('../db/connection');
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
+
+beforeEach(() => {
+  return seed(data);
+})
+
+afterAll(() => {
+  return db.end();
+})
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
