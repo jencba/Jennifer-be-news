14217e3cbc1501a5529f844d7b424651a87df748
jencba <jdamian@elam.co.uk>
2024-11-25 14:06:34 +0000

    3-get/api/articles complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -33,7 +33,6 @@ describe("GET /api/topics", () => {
       .get("/api/topics")
       .expect(200)
       .then(({ body: { topics } }) => {
-       console.log(topics)
         expect(topics).toHaveLength(3)
         topics.forEach((topic) => {
           expect(topic).toMatchObject(
@@ -42,6 +41,46 @@ describe("GET /api/topics", () => {
             }
           )
         })
+      })
+  })
+})
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: Responds with an article object each with the correct properties", () => {
+    return request(app)
+      .get('/api/articles/1')  
+      .expect(200)
+      .then(({ body: { article } }) => {
+        expect(article).toMatchObject
+        ({
+            author: expect.any(String),
+            title: expect.any(String),
+            article_id: expect.any(Number),
+            body: expect.any(String),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String)
+          })
+       
+      })
+  })
+
+  test("404: if articles doesnt exist, respond with error", () => {
+    return request(app)
+      .get('/api/articles/999999')
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe('article not found')
+      })
+  })
+
+  test("400: if given invaid id respond with error", () => {
+    return request(app)
+      .get('/api/articles/bananas')
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe('invalid article id')
       })
   })
 })
--- models/api.model.js
+++
@@ -1,9 +0,0 @@
-const {getApi}=require('./api.controller')
-const express = require('express')
-const app = express();
-
-
-app.get('/api/',getApi)
-
-
-module.exports=app
