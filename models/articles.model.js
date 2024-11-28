97244bb0f3343db1e34584959f460fc4ca43c882
jencba <jdamian@elam.co.uk>
2024-11-28 10:21:09 +0000

    11-get api articles sort complete

--- models/articles.model.js
+++ models/articles.model.js
@@ -11,7 +11,18 @@ exports.fetchArticleById = (article_id) => {
     })
 }
 
-exports.fetchAllArticles = () => {
+exports.fetchAllArticles = (sort_by = 'created_at', order = 'desc') => {
+const sortByOptions = [
+        'article_id', 'author', 'title', 'topic', 
+        'created_at', 'votes', 'article_img_url', 'comment_count'
+      ]
+       const orderOptions = ['asc', 'desc']
+    
+      if (!sortByOptions.includes(sort_by) ||!orderOptions.includes(order) ) {
+        return Promise.reject({ status: 400, msg: 'Not Valid' })   
+      }
+    
+      
     return db.query(`
         SELECT articles.article_id, articles.author, articles.title, articles.topic, 
                articles.created_at, articles.votes, articles.article_img_url, 
@@ -19,13 +30,14 @@ exports.fetchAllArticles = () => {
         FROM articles
         LEFT JOIN comments ON comments.article_id = articles.article_id
         GROUP BY articles.article_id
-        ORDER BY articles.created_at DESC;
+        ORDER BY ${sort_by} ${order.toUpperCase()};
       `)
       .then(({ rows }) => {
-    
         return rows
       })
     }
+    
+   
 
 exports.updateArticleVotes = (articleId, incVotes) => {
         return db
